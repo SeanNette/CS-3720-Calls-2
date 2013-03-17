6d1fe535275356fee0f15fcbd4374417740c124a
Dariusz <d2zborowski@yahoo.ca>
2013-03-17 14:07:27 -0600

    created main folder
    Bla bla

--- CallsProject/src/Broker/DatabaseBroker.java
+++ CallsProject/src/Broker/DatabaseBroker.java
@@ -17,34 +17,34 @@ import java.util.LinkedList;
 import java.util.Queue;
 //import java.sql.Statement;
 
-public class DatabaseBroker 
-{   
+public class DatabaseBroker
+{
     String host = "jdbc:mysql://localhost:3306/callsdb";
     String username = "root";
-    String password = "password";
-    
+    String password = "admin";
+
     Queue<Connection> connectionPool = new LinkedList<>();
-    
+
     // constructor
     /**
      *
      */
-    public DatabaseBroker() {          
+    public DatabaseBroker() {
         initializePool();
     }
-    
+
     public DatabaseBroker(String host, String username, String password) {
         this.host = host;
         this.username = username;
         this.password = password;
         //initializePool();
     }
-    
+
     // adds to pool
     private void initializePool() {
         addToPool();
     }
-    
+
     // if pool is not maximum value set and add a new connection
     private void addToPool() {
         while (!checkIfPoolFull()) {
@@ -53,19 +53,19 @@ public class DatabaseBroker
         }
         System.out.println("Pool allocated to maximum capacity " + connectionPool.size() + "." );
     }
-    
+
     // creates the new connection
     private Connection createNewConnection() {
         Connection connect = null;
-        try {            
-            connect = DriverManager.getConnection(host, username, password);            
-           // System.out.println("Connection established."); 
+        try {
+            connect = DriverManager.getConnection(host, username, password);
+           // System.out.println("Connection established.");
         } catch (SQLException err) {
             System.out.println(err.getMessage());
         }
         return connect;
     }
-    
+
     // set maximum pool and check if that is reached
     private boolean checkIfPoolFull() {
         final int MAX_POOL = 5;
@@ -74,24 +74,24 @@ public class DatabaseBroker
         }
         return true;
     }
-    
+
     // use connection from pool
     public Connection getConnectionFromPool() {
         Connection connect = null;
         if (connectionPool.size() > 0) {
             connect = connectionPool.poll();
             System.out.println("Removed connection from pool" + " " + (connectionPool.size()+1));
-        }    
-        
-        else 
+        }
+
+        else
             System.out.println("Max pool size reached" + " " + connectionPool.size());
         return connect;
     }
-    
+
     // once connection used put back in to pool
     public void returnConnectionToPool(Connection connect) {
         connectionPool.add(connect);
         System.out.println("Added connection back to pool " + connectionPool.size());
-    } 
-            
+    }
+
 }
